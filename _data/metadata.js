58082f99c48ae765db878ca8c9ad4ac1b374f24d
AdamJ <adam.jolicoeur@gmail.com>
2024-11-20 10:04:22 -0500

    Initial commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "AdamJ's Blog",
+	url: "https://www.adamjolicoeur.com/Blog",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "A blog for all thoughts of AdamJ.github.io.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Adam Jolicoeur",
+		email: "design@adamjolicoeur.com",
+		url: "https://www.adamjolicoeur.com/about"
 	}
 }
